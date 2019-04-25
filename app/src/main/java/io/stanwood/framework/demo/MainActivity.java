cbf1bb4ec6605f612f08c88003a1174686267762
aclassen <andreclassen1337@gmail.com>
2019-04-25 03:48:05 +0200

    Updated to material components

--- app/src/main/java/io/stanwood/framework/demo/MainActivity.java
+++ app/src/main/java/io/stanwood/framework/demo/MainActivity.java
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidInjection.inject(this);
-
         setContentView(R.layout.activity_main);
         if (ratingService.shouldBeDisplayed()) {
             RatingDialog.builder()
